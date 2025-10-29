04073dee91e0af404b706d18c5d56d94a12d5416
softmnet <softmnet@gmail.com>
2025-10-29 22:01:18 +0900

    Del

--- 내 노트 (21)/배치파일 기초 작성법 files/svn_commit_editplus.bat
+++
@@ -1,7 +0,0 @@
-rem @echo off
-pushd %~DP0
-d:
-cd D:\Work\editplus_workspace
-svn cleanup
-svn commit --message editplus
-popd
--- 내 노트 (21)/배치파일 기초 작성법 files/svn_update_editplus.bat
+++
@@ -1,6 +0,0 @@
-rem @echo off
-pushd %~DP0
-d:
-cd D:\Work\editplus_workspace
-svn update
-popd
